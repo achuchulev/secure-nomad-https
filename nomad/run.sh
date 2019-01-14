881f800dcdba50d117ee3b0a04fc983059508e5c
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-01-14 16:26:36 +0200

    Update run.sh

--- nomad/run.sh
+++ nomad/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Run Nomad server & cient in background
-nomad agent -config /vagrant/config/server1.hcl > /dev/null 2>&1
+nomad agent -config /vagrant/config/server1.hcl &> /dev/null 2>&1
 #nomad agent -config /vagrant/config/client1.hcl > /dev/null 2>&1
