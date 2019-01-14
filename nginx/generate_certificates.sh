ed2a8b20eee4f2fddd1da67365c5f568506ebf4e
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-01-14 14:59:40 +0200

    Update generate_certificates.sh

--- nginx/generate_certificates.sh
+++ nginx/generate_certificates.sh
@@ -4,8 +4,8 @@ sudo mkdir /etc/nginx/ssl
 cd /etc/nginx/ssl
 
 # Generate the CA's private key and certificate
-cfssl print-defaults csr | cfssl gencert -initca - | cfssljson -bare nginx-ca
+sudo cfssl print-defaults csr | cfssl gencert -initca - | cfssljson -bare nginx-ca
 
 # Generate a certificate for the nginx server
-echo '{}' | cfssl gencert -ca=nginx-ca.pem -ca-key=nginx-ca-key.pem -config=/vagrant/config/cfssl.json \
+sudo echo '{}' | cfssl gencert -ca=nginx-ca.pem -ca-key=nginx-ca-key.pem -config=/vagrant/config/cfssl.json \
     -hostname="localhost,127.0.0.1" - | cfssljson -bare server
