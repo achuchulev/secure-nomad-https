ece01a7b438869a20db88a0d3b8bbd3fe6e513d4
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-01-14 14:52:43 +0200

    Update configure.sh

--- nginx/configure.sh
+++ nginx/configure.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # stop nginx service
-systemctl stop nginx.service
+sudo systemctl stop nginx.service
 
 # remove default conf of nginx
 [ -f /etc/nginx/sites-available/default ] && {
@@ -9,10 +9,10 @@ systemctl stop nginx.service
 }
 
 # copy our nginx conf
-cp nginx/nginx.conf /etc/nginx/sites-available/default
+sudo cp nginx/nginx.conf /etc/nginx/sites-available/default
 
 # start nginx service
-systemctl start nginx.service
+sudo systemctl start nginx.service
 
 # check status of nginx service
 systemctl status nginx.service
