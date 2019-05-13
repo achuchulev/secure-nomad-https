bb0db0732fd3678ffc8c093a308fa9463ecd9efc
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-05-13 15:57:57 +0300

    Update tools.sh

--- dev/scripts/tools.sh
+++ dev/scripts/tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-systemctl stop apt-daily.service
-systemctl kill --kill-who=all apt-daily.service
+sudo systemctl stop apt-daily.service
+sudo systemctl kill --kill-who=all apt-daily.service
 # wait until `apt-get updated` has been killed
 while ! (systemctl list-units --all apt-daily.service | fgrep -q dead)
 do
